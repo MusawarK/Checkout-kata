8ac0b2296531546c7ed9855833e37c430796b664
MusawarK <musawar.khan@techamus.com>
2025-06-01 18:38:12 +0100

    GREEN: Return 0 when basket is empty – hardcoded response

--- checkout/Checkout.DLL/Checkout.cs
+++ checkout/Checkout.DLL/Checkout.cs
@@ -8,9 +8,15 @@ namespace Checkout.DLL
 {
     public class Checkout : ICheckout
     {
+        private readonly Dictionary<string, int> _items;
+
+        public Checkout() {
+            _items = new Dictionary<string, int>();
+        }
+
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            return 0;
         }
 
     }
